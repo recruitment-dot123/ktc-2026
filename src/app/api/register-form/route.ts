10b8dccf263fcbca3f70e5968b79ca5b4cb8c483
recruitment-dot123 <recruitment@likelion.net>
2026-05-27 12:45:03 +0700

    Update route.ts

--- src/app/api/register-form/route.ts
+++ src/app/api/register-form/route.ts
@@ -3,20 +3,23 @@ import { doc } from "@/lib/spreadsheet";
 
 export async function POST(request: Request) {
   const data = await request.json();
-
   const registerValidator = registerPostSchema.safeParse(data);
-
   if (!registerValidator.success)
     return Response.json("Invalid data", {
       status: 400,
     });
-
   try {
     await doc.loadInfo();
     const sheet = doc.sheetsByIndex[0];
+    const { utm_source, utm_medium, utm_campaign, utm_content, ...rest } =
+      registerValidator.data;
     await sheet.addRow({
-      ...registerValidator.data,
+      ...rest,
       date: new Date().toISOString(),
+      utm_source: utm_source ?? "",
+      utm_medium: utm_medium ?? "",
+      utm_campaign: utm_campaign ?? "",
+      utm_content: utm_content ?? "",
     });
     return Response.json("OK");
   } catch (error: any) {
